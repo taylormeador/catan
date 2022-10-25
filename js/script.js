1e9b009a1bc8851605b9f22ba1c29572d2598a16
Taylor Meador <taylor.r.meador@gmail.com>
2022-10-24 18:04:25 -0600

    Experiment with drawing board hexes

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const hexes = document.querySelectorAll(".hex");
+
+// for (let i = 0; i < hexes.length; i++) {
+//     hex = hexes[i]
+//     if (hex.classList.contains('two')) {
+//         hex.textContent = '2';
+//     }
+// }
